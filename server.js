cbc84488fed7a344bdac07707b3b76bc2b67645e
imanomura <206794822+imanomura@users.noreply.github.com>
2025-11-07 12:03:18 +0900

    レコードの単体取得

--- server.js
+++ server.js
@@ -1,1 +1,10 @@
 // 今回はクライアントサイドもルーティングもないので何もimportしない
+const kv = await Deno.openKv();
+
+await kv.set(['pokemon', 'ブラッキー'], { type: '悪', lebel: 35 });
+await kv.set(['pokemon', 'シャワーズ'], { type: '水', level: 26 });
+await kv.set(['ブラッキー'], { type: '悪', level: 35 });
+
+const pkmn = await kv.get(['pokemon', 'ブラッキー']);
+console.log(pkmn.key);
+console.log(pkmn.value);
